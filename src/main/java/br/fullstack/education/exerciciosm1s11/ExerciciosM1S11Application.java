e1e744910c0b6744cb8cb3bd73d06f8f8f5547b1
CherylHenkels <cherylhenkelss@gmail.com>
2024-04-22 11:46:26 -0300

    Criação do projeto

---
+++ src/main/java/br/fullstack/education/exerciciosm1s11/ExerciciosM1S11Application.java
@@ -0,0 +1,13 @@
+package br.fullstack.education.exerciciosm1s11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosM1S11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciciosM1S11Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/fullstack/education/exerciciosm1s11/ExerciciosM1S11ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fullstack.education.exerciciosm1s11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosM1S11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
